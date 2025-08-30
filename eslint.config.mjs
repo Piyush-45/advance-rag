429c6fccdaa794fba7412f6f7a30b29a92c46e7d
piyush tyagi <tyagipiyush2233@gmail.com>
2025-08-31 00:06:34 +0530

    type error fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,12 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+      files: ["src/app/api/**/*.{ts,tsx}"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+      },
   },
+
 ];
 
 export default eslintConfig;
